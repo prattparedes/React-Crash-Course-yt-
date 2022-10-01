6d7d377818a372ab783ef96b2bb2c66accb8d88e
Pratt <pratt.paredes@gmail.com>
2022-10-01 14:30:45 -0500

    conditional

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import './App.css';
 
 const App = () => {
   const name = 'John';
+  const isNameShowing = true;
   
   return (
     <div className="App">
-      <h1>Hello, {name}!</h1>
+      <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
+      {name ? (
+      <>
+        test
+      </>) : (
+        test
+      )}
     </div>
   );
 }
