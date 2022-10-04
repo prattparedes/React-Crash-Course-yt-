d2eef02da59b825c7c1e78277a2132b85d50fc22
Pratt <pratt.paredes@gmail.com>
2022-10-03 22:19:05 -0500

    using states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.LastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
-
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <Person name={'John'} LastName={'Doe'} age={25}/>
-      <Person name={'Mary'} LastName={'Jane'} age={22}/>
-      <Person name={'Bryan'} LastName={'Wick'} age={20}/>
-      <Person name={'Ted'} LastName={'Mais'} age={27}/>
-      <Person name={'Gen'} LastName={'Kim'} age={19}/>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
